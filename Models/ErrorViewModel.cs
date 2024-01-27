0e0ea2e00b42bf31c53314b7975bc10aeedf503e
Teddy <dudetotheleft@gmail.com>
2024-01-26 16:21:13 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CuppaComfort.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
