1dbac1ac3d5cfad24238e79c91f9b6e194e8f1d4
Teddy <dudetotheleft@gmail.com>
2024-01-28 23:03:24 -0800

    Added data annotations to all models

--- Models/Reward.cs
+++ Models/Reward.cs
@@ -1,15 +1,20 @@
-﻿namespace CuppaComfort.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CuppaComfort.Models
 {
     public class Reward
     {
         /// <summary>
         /// The unique ID of the reward
         /// </summary>
+        [Key]
         public int RewardId { get; set; }
-        
+
         /// <summary>
         /// The code that will be used to utilize the reward
         /// </summary>
+        [Required]
         public string Code { get; set; }
         
         /// <summary>
@@ -17,6 +22,8 @@
         /// Personal reward codes can be discarded and recycled after use
         /// Universal rewards should not be discarded
         /// </summary>
+        [Required]
+        [DisplayName("Reward Type")]
         public string RewardType { get; set; }
 
 
@@ -25,11 +32,14 @@
         /// <summary>
         /// Whether or not the reward uses percentage format
         /// </summary>
+        [Required]
+        [DisplayName("Percentage?")]
         public bool IsPercentage { get; set; }
 
         /// <summary>
         /// If the reward is a percentage, this is the percentage
         /// </summary>
+        [DisplayName("Discount Percentage")]
         public int DiscountPercentage { get; set; }
 
 
@@ -37,11 +47,14 @@
         /// <summary>
         /// Whether or not the reward uses a flat amount format
         /// </summary>
+        [Required]
+        [DisplayName("Flat Amount?")]
         public bool IsFlatAmount { get; set; }
 
         /// <summary>
         /// If the reward is a flat amount, this is the amount
         /// </summary>
+        [DisplayName("Discount Amount")]
         public int DiscountAmount { get; set; }
 
 
@@ -49,6 +62,8 @@
         /// <summary>
         /// Whether or not the reward is a free waiver
         /// </summary>
+        [Required]
+        [DisplayName("Free Waiver?")]
         public bool IsFreeWaiver { get; set; }
 
     }
