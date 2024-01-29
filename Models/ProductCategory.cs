8caf8592d7b3f53cc37baf5a25b6942ff84fbb15
Teddy <dudetotheleft@gmail.com>
2024-01-28 20:11:20 -0800

    Created product models

---
+++ Models/ProductCategory.cs
@@ -0,0 +1,23 @@
+﻿namespace CuppaComfort.Models
+{
+    /// <summary>
+    /// Represents the category of a product
+    /// </summary>
+    public enum ProductCategory
+    {
+        // Drink categories
+        Coffee,
+        Tea,
+        Energy,
+
+        // Snack categories
+        Hot,
+        Cold,
+
+        // Drinkware categories
+        Standard,
+        Insulated,
+        Mugs,
+        Other
+    }
+}
